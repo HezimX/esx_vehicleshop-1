9fbd7e963d3c18a6f7c202f5ad8fe67dfcfa4b7b
Parow <rya4an@gmail.com>
2019-08-24 17:53:02 +0200

    NativeUI push

--- __resource.lua
+++ __resource.lua
@@ -1,35 +1,15 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+server_script '@mysql-async/lib/MySQL.lua'
+client_script "NativeUI.lua"
+client_script 'cl_vehshop.lua'
+server_script 'sv_vehshop.lua'
 
-description 'ESX Vehicle Shop'
 
-version '1.1.0'
 
-server_scripts {
-	'@mysql-async/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'config.lua',
-	'server/main.lua'
-}
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
 	'config.lua',
 	'client/utils.lua',
 	'client/main.lua'
 }
-
-dependency 'es_extended'
-
-export 'GeneratePlate'
