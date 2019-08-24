d69c9cce6f2d0bc74cb9fbd4904a767e29900c3c
Parow <rya4an@gmail.com>
2019-08-24 17:53:49 +0200

    reformat

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,8 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 server_script '@mysql-async/lib/MySQL.lua'
-client_script "NativeUI.lua"
-client_script 'cl_vehshop.lua'
-server_script 'sv_vehshop.lua'
+client_script "client/NativeUI.lua"
+client_script 'client/cl_vehshop.lua'
+server_script 'server/sv_vehshop.lua'
 
 
 
